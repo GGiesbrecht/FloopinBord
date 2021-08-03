fec9e3cba10657b0b2aca59e7b1c3df0197fea77
Garron <garron@cholakisdental.com>
2021-08-03 11:26:04 -0500

    fade to play scene works

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +10,13 @@ public class GameManager : MonoBehaviour
     public void Play() {
         birdAnimator.SetBool("GameStarted", true);
         playButtonAnimator.SetBool("GameStarted", true);
+    }
+
+    public void FadeToBlack() {
         blackScreenAnimator.SetBool("GameStarted", true);
+    }
+
+    public void LoadNextScene() {
+        SceneManager.LoadScene("Play");
     }
 }
